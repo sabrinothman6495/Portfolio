4f2901601f6dbdb0fce5a4bbf1f05d669b9dc597
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-10-05 10:42:36 -0400

    about me page uploaded

---
+++ Student Portfolio/src/Pages/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//TODO: Import the profile picture
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src={profilePic} alt="Profile Pic" />
+//TODO: add paragraph      <p></p>
+    </section>
+  );
+}
+
+export default AboutMe;

5510b72e94486a65a3e06e5215a47c082205ebb5
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-10-08 14:51:00 -0400

    update to readme and css

--- Student-Portfolio/src/components/Header.jsx
+++ Student-Portfolio/src/components/Header.jsx
@@ -4,14 +4,16 @@ import './Header.css';
 
 function Header() {
   return (
-    <header className="App-header">
-      <nav>
-        <ul className="nav-links">
+    <header className="Header">
+      <div className="logo">
+      <nav className='header_nav'>
+        <ul>
           <li><Link to="/AboutMe">About Me</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/projects">Projects</Link></li>
         </ul>
       </nav>
+      </div>
     </header>
   );
 }
